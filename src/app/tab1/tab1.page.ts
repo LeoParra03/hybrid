48737523cd026c4cba4d61305ee32f6598540621
kevinMaga <kerimaga@espol.edu.ec>
2024-08-14 09:31:17 -0500

    Page3

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -4,6 +4,7 @@ import { IonHeader, IonToolbar, IonTitle, IonContent, IonNavLink, IonButton, Ion
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { PageOneComponent } from '../page1/page1.component';
 import { PageTwoComponent } from '../page2/page2.component';
+import { PageThreeComponent } from '../page3/page3.component';
 
 @Component({
   selector: 'app-tab1',
@@ -15,6 +16,7 @@ import { PageTwoComponent } from '../page2/page2.component';
 export class Tab1Page {
   component = PageOneComponent;
   component2 = PageTwoComponent;
+  component3 = PageThreeComponent;
   onClick() {
     console.log('Button clicked!');
   }
